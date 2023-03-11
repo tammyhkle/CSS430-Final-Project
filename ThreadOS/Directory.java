5998b221daeb829d60c5aaf849b39ba3da44c359
lordmangos <shaykho@uw.edu>
2023-03-10 17:59:50 -0800

    finished directory()

--- ThreadOS/Directory.java
+++ ThreadOS/Directory.java
@@ -7,28 +7,28 @@ public class Directory {
    private char fnames[][];           // each element stores a different file name.
 
    public Directory( int maxInumber ) {               // directory constructor
-      fsizes = new int[maxInumber];                   // maxInumber = max files
+      fsize = new int[maxInumber];                   // maxInumber = max files
       for ( int i = 0; i < maxInumber; i++ ) {
          fsize[i] = 0;                                // all file size initialized to 0
       }
       fnames = new char[maxInumber][maxChars];
       String root = "/";                              // entry(inode) 0 is "/"
       fsize[0] = root.length( );                      // fsize[0] is the size of "/".
-      root.getChars( 0, fsizes[0], fnames[0], 0 );    // fnames[0] includes "/"
+      root.getChars( 0, fsize[0], fnames[0], 0 );    // fnames[0] includes "/"
    }
 
    public void bytes2directory( byte data[] ) {
       // assumes data[] contains directory information retrieved from disk 
-      // initialize the directory fsizes[] and fnames[] with this data[] 
+      // initialize the directory fsize[] and fnames[] with this data[] 
       
       int offset = 0;
-      for ( int i = 0; i < fsizes.length; i++, offset += 4 ) {
-         fsizes[i] = SysLib.bytes2int( data, offset );
+      for ( int i = 0; i < fsize.length; i++, offset += 4 ) {
+         fsize[i] = SysLib.bytes2int( data, offset );
       }
       
       for ( int i = 0; i < fnames.length; i++, offset += maxChars * 2 ) { 
          String fname = new String( data, offset, maxChars * 2 );
-         fname.getChars( 0, fsizes[i], fnames[i], 0 ); 
+         fname.getChars( 0, fsize[i], fnames[i], 0 ); 
       }
    }
 
@@ -36,13 +36,13 @@ public class Directory {
       // converts and return directory information into a plain byte array
       // this byte array will be written back to disk
       
-      byte[] data = new byte[fsizes.length * 4 + fnames.length * maxChars * 2]; 
+      byte[] data = new byte[fsize.length * 4 + fnames.length * maxChars * 2]; 
       int offset = 0;
-      for ( int i = 0; i < fsizes.length; i++, offset += 4 ) {
-         SysLib.int2bytes( fsizes[i], data, offset );
+      for ( int i = 0; i < fsize.length; i++, offset += 4 ) {
+         SysLib.int2bytes( fsize[i], data, offset );
       }
       for ( int i = 0; i < fnames.length; i++, offset += maxChars * 2 ) { 
-         String tableEntry = new String( fnames[i], 0, fsizes[i] ); 
+         String tableEntry = new String( fnames[i], 0, fsize[i] ); 
          byte[] bytes = tableEntry.getBytes( );
          System.arraycopy( bytes, 0, data, offset, bytes.length );
       }
@@ -50,23 +50,46 @@ public class Directory {
       return data; 
    }
 
+   // filename is the one of a file to be created. 
+   // allocates a new inode number for this filename
    public short ialloc(String filename) {
-      // filename is the one of a file to be created. 
-      // allocates a new inode number for this filename
-      short s = 0;   // I added
-      return s;
+      int file;
+      for (short i = 0; i < fsize.length; i++) {
+         if (fsize[i] == 0) {
+            if (filename.length() > maxChars) {
+               file = maxChars;
+            } else {
+               file = filename.length();
+            }
+            fsize[i] = file;
+            filename.getChars(0, fsize[i], fnames[i], 0);
+            return i;
+         }
+      }
+      return -1;
    }
 
+   // deallocates this inumber (inode number) 
+   // the corresponding file will be deleted.
    public boolean ifree(short iNumber) {
-      // deallocates this inumber (inode number) 
-      // the corresponding file will be deleted.
-      return true;   // I added
+      if (fsize[iNumber] > 0 && iNumber < maxChars) {  // check if iNum valid
+         fsize[iNumber] = 0;                           // delete corresponding file
+         return true;
+      }
+      return false;
    }
 
+   // returns the inumber corresponding to this filename
    public short namei(String filename) {
-      // returns the inumber corresponding to this filename
-      short s = 0;   // I added
-      return s;
+      for (short i = 0; i < fsize.length; i++){
+         if (fsize[i] == filename.length()) {
+            String temp = new String(fnames[i], 0, fsize[i]);
+            if (filename.equals(temp)) {
+               return i;
+            }
+         }
+      }
+     return -1;
    }
 
 }
